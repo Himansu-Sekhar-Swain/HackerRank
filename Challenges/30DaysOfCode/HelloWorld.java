3ac8309f6e65a7031b13c34a3d2484c38311cd64
Himansu-Sekhar-Swain <himansu.sekhar.swain@gmail.com>
2019-09-29 21:48:09 +0530

    Hello World

--- Challenges/30DaysOfCode/HelloWorld.java
+++ Challenges/30DaysOfCode/HelloWorld.java
@@ -4,6 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 public class Solution {
+	
   public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in); 
 		String inputString = scan.nextLine(); 
