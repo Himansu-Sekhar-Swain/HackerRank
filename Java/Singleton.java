3d08f57b04a45556c93e8a297385aef6d1ec4a28
Himansu-Sekhar-Swain <himansu.sekhar.swain@gmail.com>
2019-09-30 10:04:52 +0530

    Java Singleton Pattern

---
+++ Java/Singleton.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+class Singleton {
+    public static Singleton singleton;
+    public String str;
+    private Singleton() {}
+    public static Singleton getSingleInstance() {
+        if(singleton == null)
+            singleton = new Singleton();
+        return singleton;
+    }
+}
