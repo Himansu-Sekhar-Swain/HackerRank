4257143c9df6eeecb9d8f6615a5c27604bdc85ce
Himansu <himansu.sekhar.swain@gmail.com>
2019-10-04 16:51:00 +0530

    Day 1 : Data Type

--- 30DaysOfCode/Day1DataTypes.java
+++ 30DaysOfCode/Day1DataTypes.java
@@ -16,14 +16,18 @@ public class Solution {
         int i2 = scan.nextInt();
         double d2 = scan.nextDouble();
         String s2 = "";
+	    
         //this is important to read the next line
         while(scan.hasNext()) {
             s2 = scan.nextLine();
         }       
 
         i2 = i2 + i;
+	    
         System.out.println(i2);
+	    
         d2 = d2 + d;
+	    
         System.out.printf("%.1f", d2);
         System.out.println("\n"+s+s2);
 
