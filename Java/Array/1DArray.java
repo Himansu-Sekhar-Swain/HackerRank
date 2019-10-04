f34e682773648890d76bbede4b83bbd4496059bc
Himansu <himansu.sekhar.swain@gmail.com>
2019-10-04 16:06:54 +0530

    1D Array

---
+++ Java/Array/1DArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++) {
+            int x = scan.nextInt();
+            a[i] = x;
+        }
+        //Arrays.sort(a);
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
