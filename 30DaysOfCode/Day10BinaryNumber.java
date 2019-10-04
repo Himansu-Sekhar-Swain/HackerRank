65717a32c81f264c91b36a2a28d54204ecc9e611
Himansu <himansu.sekhar.swain@gmail.com>
2019-10-04 16:48:19 +0530

    Day 10 : Binary Numbers

--- 30DaysOfCode/Day10BinaryNumber.java
+++ 30DaysOfCode/Day10BinaryNumber.java
@@ -7,31 +7,24 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class Solution {
-
     private static final Scanner scanner = new Scanner(System.in);
     
+    /**
+    * Create map and add repeating values
+    */
     private static int getMaxConsucative(List<Integer> list) {
         int count = 1;
         Map<Integer, Integer> map = new LinkedHashMap<Integer, Integer>();
-        //List<Integer> outList = new ArrayList<Integer>();        
         for(int i = 0; i < list.size() - 1; i++) {            
             if(list.get(i) == list.get(i + 1)) {
                 count++;
-                //outList.add(count);
-                //System.out.println(list.get(i)+"> "+count);                        
-                //System.out.println("Map > "+map);
                 if(map.get(list.get(i)) != null) {                    
                     if(count > map.get(list.get(i)))
                         map.put(list.get(i), count);
                 } else 
                     map.put(list.get(i), count);
-                
             } else {
                 count = 1;
-                //outList.add(count);
-                //System.out.println(list.get(i)+">>> "+count);
-                //map.put(list.get(i), count);
-                //System.out.println("Map >>> "+map);
                 if(map.get(list.get(i)) != null) {
                     if(count > map.get(list.get(i)))
                         map.put(list.get(i), count);
@@ -39,13 +32,19 @@ public class Solution {
                     map.put(list.get(i), count);
             }
         }        
-        //System.out.println(map);
         return Collections.max(map.values());
     }
+    
+    /** 
+    * Main Method
+    */
     public static void main(String[] args) {
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
         List<Integer> list = new ArrayList<>();
+        /**
+        * Collect all reminder of a number after divided by 2 and then find the max. Consucative
+        */
         do {
             list.add(n % 2);
             n = n / 2;            
