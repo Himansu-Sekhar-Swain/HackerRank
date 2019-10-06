50d9285e49ac0ef8210540ed8c951acc4ab635be
Himansu <himansu.sekhar.swain@gmail.com>
2019-10-07 00:43:05 +0530

    Insert Data at Head in Linked List

--- Java/Data Structure/InsertDataInLinkedList.java
+++ Java/Data Structure/InsertDataInLinkedList.java
@@ -27,18 +27,14 @@ public class Solution {
         public SinglyLinkedList() {
             this.head = null;
             this.tail = null;
-        }
-
-      
+        }      
     }
     
     //Class was provided by Hackerrank
     public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
         while (node != null) {
             bufferedWriter.write(String.valueOf(node.data));
-
             node = node.next;
-
             if (node != null) {
                 bufferedWriter.write(sep);
             }
@@ -49,42 +45,31 @@ public class Solution {
      * Implemented Method
      */
     static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode head, int data) {
-
         SinglyLinkedListNode temp = head;
         SinglyLinkedListNode first = new SinglyLinkedListNode(data);
         head = first;
         head.next = temp;
 
         return head;
-
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
         SinglyLinkedList llist = new SinglyLinkedList();
-
         int llistCount = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         for (int i = 0; i < llistCount; i++) {
             int llistItem = scanner.nextInt();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        	
-          SinglyLinkedListNode llist_head = insertNodeAtHead(llist.head, llistItem);
-
-          llist.head = llist_head;
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");        	
+            SinglyLinkedListNode llist_head = insertNodeAtHead(llist.head, llistItem);
+            llist.head = llist_head;
         }
-
-
-
         printSinglyLinkedList(llist.head, "\n", bufferedWriter);
         bufferedWriter.newLine();
-
         bufferedWriter.close();
-
         scanner.close();
     }
 }
