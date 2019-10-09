98ce9e10bbd2c30e3e6f632c6f5ff6332ea62c81
Himansu <himansu.sekhar.swain@gmail.com>
2019-10-09 16:24:53 +0530

    Java Cryptographic [SHA-256]

---
+++ Java/Advanced Java/Cryptographic_SHA256.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;  
+import java.nio.charset.StandardCharsets; 
+import java.security.MessageDigest;  
+import java.security.NoSuchAlgorithmException;
+
+public class Solution {
+    /**
+    * Below method prints the hash value
+    */
+    static void printSHA_256_Hash(String input) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] bytes = md.digest(input.getBytes(StandardCharsets.UTF_8));
+        BigInteger bigInt = new BigInteger(1, bytes);
+        StringBuilder sb = new StringBuilder(bigInt.toString(16));
+        while(sb.length() < 32) {
+            sb.insert(0, "0");
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        //System.out.println(input);
+        try {
+            printSHA_256_Hash(input);
+        } catch(Exception e) {}       
+
+    }
+}
+
