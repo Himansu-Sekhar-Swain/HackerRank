bd376f8eb4b6692c8bc13856b9e96084382c7626
Himansu <himansu.sekhar.swain@gmail.com>
2019-10-11 15:44:21 +0530

    Day 19 Interface [Sum of All Divisors]

---
+++ 30DaysOfCode/Day19Interface.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+class Calculator implements AdvancedArithmetic {
+
+    public int divisorSum(int n) {
+        int sum = 0;
+        for(int x = 1; x <= n; x ++ ) {
+            if(n % x == 0) { //Get all divisors
+                sum = sum + x;
+            }
+        }
+        return sum;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
