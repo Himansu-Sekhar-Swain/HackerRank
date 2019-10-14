ea755be7ef817cccf553718d039558bf9473b573
Himansu <himansu.sekhar.swain@gmail.com>
2019-10-14 13:09:37 +0530

    Day 21 Generic

---
+++ 30DaysOfCode/Day21Generic.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Printer <T> {
+
+    /**
+    * Best way of handling Generic Arrays is convert it to Collection Objects.
+    * All Collection Objects will manipulate items using 'AutoBoxing' mechanism.
+    * 
+    **/
+    
+    public void printArray(Object[] arr) {
+        List<Object> list = Arrays.asList(arr); 
+        for(Object o : list) {
+            System.out.println(o); //Auto Boxing [No Need to Handle Any Object Type Here]
+        }
+    }
+}
+
+public class Generics {
+    
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+        
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    } 
+}
